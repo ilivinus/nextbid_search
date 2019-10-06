6fa77502c82bf832a1f190f0a0c529db504a31d9
Umeaduma Livinus <umeadumalivinus@gmail.com>
2019-10-06 01:29:05 +0100

    removal of redux related modules*

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
 import Search from '../search';
 import './app.css';
-// import { Provider } from 'react-redux';
-// import store from '../../redux';
-
 
 
 export default class App extends Component {
     render(){
         return (
             <div className="App">
-             {/* <Provider store={store}> */}
                 <Search />
-            {/* </Provider> */}
             </div>
             );
     }

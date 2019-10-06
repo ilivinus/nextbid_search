9be23e99cb0e75c36aaabaf32e19ebb3c88d0c68
Umeaduma Livinus <umeadumalivinus@gmail.com>
2019-10-06 15:28:14 +0100

    changed api http to https to allow access from chrome

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,5 @@
 export default {
-    baseUrl : "http://geodata.nationaalgeoregister.nl/locatieserver/v3/suggest",
+    baseUrl : "https://geodata.nationaalgeoregister.nl/locatieserver/v3/suggest",
     defaultQueryString : "?fq=type:adres&start=0&rows=20&q={0}&fq=*:*",
     paginatedQueryString : "?fq=type:adres&start={0}&rows={1}&q={2}&fq=*:*",
     debounceWaitTime : 250,
